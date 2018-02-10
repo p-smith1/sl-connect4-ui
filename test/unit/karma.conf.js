0384c32ae502c6a299abf38996bb63bdcf750270
Patrick Smith <patrick.smith@kashew.net>
2018-02-10 11:23:51 -0500

    Testing Restructuring * Changed Karma to use ChromeHeadless * Updated all NPM packages to latest * Reorganized spec structure * Added @vue/test-utils for component-level testing

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -5,16 +5,21 @@
 
 var webpackConfig = require('../../build/webpack.test.conf')
 
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
 module.exports = function karmaConfig (config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
+    browsers: ['ChromeHeadless'],
     frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
-    files: ['./index.js'],
+    files: [
+      '../../node_modules/babel-polyfill/dist/polyfill.js',
+      './index.js'
+    ],
     preprocessors: {
       './index.js': ['webpack', 'sourcemap']
     },
