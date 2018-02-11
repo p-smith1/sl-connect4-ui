caf868a360376ca38a901ff836e52a60645be2f0
Patrick Smith <patrick.smith@kashew.net>
2018-02-11 15:49:39 -0500

    Added initial Dashboard Page component

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>Dashboard</div>
+</template>
+
+<script>
+export default {
+  name: 'Dashboard',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
