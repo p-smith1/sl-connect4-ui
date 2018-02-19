63bd56ce9c8cd757effb3c28f6a2deb66371f7f1
Patrick Smith <patrick.smith@kashew.net>
2018-02-18 20:32:00 -0500

    Added Game Actions * createGame * joinGame

--- src/store/modules/game/actions/actions.spec.js
+++ src/store/modules/game/actions/actions.spec.js
@@ -6,6 +6,10 @@ import types from '../mutations/types'
 describe('Game Module: Actions', () => {
   let vuexContext
   let axiosGetSpy
+  let axiosPostSpy
+  let axiosPutSpy
+
+  const boardsData = { foo: 'bar' }
 
   before(() => {
     moxios.install()
@@ -14,23 +18,25 @@ describe('Game Module: Actions', () => {
     sinon.spy(vuexContext, 'commit')
 
     axiosGetSpy = sinon.spy(axios, 'get')
+    axiosPostSpy = sinon.spy(axios, 'post')
+    axiosPutSpy = sinon.spy(axios, 'put')
+
+    moxios.stubRequest(`${process.env.BASE_API}/boards`, {
+      status: 200,
+      response: boardsData
+    })
   })
 
   after(() => {
     moxios.uninstall()
     axiosGetSpy.restore()
+    axiosPostSpy.restore()
+    axiosPutSpy.restore()
   })
 
   describe('#getJoinableGames', () => {
     context('when call is successful', () => {
-      const joinableBoards = [{ id: 1 }, { id: 2 }, { id: 3 }]
-
       before(async () => {
-        moxios.stubRequest(`${process.env.BASE_API}/boards`, {
-          status: 200,
-          response: joinableBoards
-        })
-
         await actions.getJoinableGames(vuexContext)
       })
 
@@ -42,8 +48,70 @@ describe('Game Module: Actions', () => {
       })
 
       it('commits the joinable games data', () => {
-        expect(vuexContext.commit).to.have.been.calledWith(types.SET_JOINABLE_GAMES, joinableBoards)
+        expect(vuexContext.commit).to.have.been
+          .calledWith(types.SET_JOINABLE_GAMES, boardsData)
       })
+    })
+  })
+
+  describe('#createGame', () => {
+    const isOpponentAi = false
+
+    before(async () => {
+      await actions.createGame(vuexContext, {isOpponentAi})
+    })
+
+    it('makes a post request', () => {
+      expect(axios.post).to.have.been
+        .calledWith(`${process.env.BASE_API}/boards`, {
+          is_opponent_ai: isOpponentAi
+        })
+    })
+
+    it('commits created game', () => {
+      expect(vuexContext.commit).to.have.been
+        .calledWith(types.SET_CURRENT_GAME, boardsData)
+    })
+  })
+
+  describe('#joinGame', () => {
+    const boardId = 5
+    const primaryPlayerId = 23
+    const userId = 42
+
+    const board = {
+      id: boardId,
+      primary_player_id: primaryPlayerId,
+      secondary_player_id: null,
+      current_player_id: null
+    }
+
+    const currentGameBoard = {
+      id: boardId,
+      primary_player_id: primaryPlayerId,
+      secondary_player_id: userId,
+      current_player_id: primaryPlayerId
+    }
+
+    before(async () => {
+      moxios.stubRequest(`${process.env.BASE_API}/boards/${boardId}`, {
+        status: 200,
+        response: board
+      })
+
+      await actions.joinGame(vuexContext, {board, userId})
+    })
+
+    it('makes a put request', () => {
+      expect(axios.put).to.have.been
+        .calledWith(`${process.env.BASE_API}/boards/${boardId}`, {
+          secondary_player_id: userId
+        })
+    })
+
+    it('commits created game', () => {
+      expect(vuexContext.commit).to.have.been
+        .calledWith(types.SET_CURRENT_GAME, currentGameBoard)
     })
   })
 })
--- src/store/modules/game/actions/index.js
+++ src/store/modules/game/actions/index.js
@@ -9,6 +9,25 @@ const actions = {
     }).then((response) => {
       context.commit(types.SET_JOINABLE_GAMES, response.data)
     })
+  },
+
+  createGame: (context, {isOpponentAi}) => {
+    return axios.post(`${process.env.BASE_API}/boards`, {
+      is_opponent_ai: isOpponentAi
+    }).then((response) => {
+      context.commit(types.SET_CURRENT_GAME, response.data)
+    })
+  },
+
+  joinGame: (context, {board, userId}) => {
+    return axios.put(`${process.env.BASE_API}/boards/${board.id}`, {
+      secondary_player_id: userId
+    }).then((response) => {
+      board.current_player_id = board.primary_player_id
+      board.secondary_player_id = userId
+
+      context.commit(types.SET_CURRENT_GAME, board)
+    })
   }
 }
 
